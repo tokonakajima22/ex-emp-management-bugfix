3528510c6f632bebfad0ca63f0eefd55ae5e2a6e
tokonakajima22 <toko.nakajima@rakus-partners.co.jp>
2021-11-16 12:05:00 +0900

    (1-2)管理者登録画面にバリデーションを追加

--- src/main/java/jp/co/sample/emp_management/form/InsertAdministratorForm.java
+++ src/main/java/jp/co/sample/emp_management/form/InsertAdministratorForm.java
@@ -1,5 +1,10 @@
 package jp.co.sample.emp_management.form;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 /**
  * 管理者情報登録時に使用するフォーム.
  * 
@@ -8,10 +13,15 @@ package jp.co.sample.emp_management.form;
  */
 public class InsertAdministratorForm {
 	/** 名前 */
+	@Size(min=1, max=20, message="名前は20文字以内にしてください")
 	private String name;
 	/** メールアドレス */
+	@NotBlank(message="メールアドレスは必須です")
+	@Email(message="Eメールの形式が不正です")
 	private String mailAddress;
 	/** パスワード */
+	@Pattern(regexp="^[0-9a-zA-Z]+$", message="英大文字、英小文字、数字で作成してください")
+	@Size(min=8, message="パスワードは8文字以上にしてください")
 	private String password;
 
 	/**
